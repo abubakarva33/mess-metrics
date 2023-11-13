9fec77c0c20ffc17b5bb09ae5b5c252310a1fd4d
Abubakar <abubakarsiddikl.info@gmail.com>
2023-11-13 22:52:01 +0600

    month delete

--- src/pages/non-shared/ManageMonths/DeleteOldMonth/DeleteOldMonth.jsx
+++ src/pages/non-shared/ManageMonths/DeleteOldMonth/DeleteOldMonth.jsx
@@ -5,21 +5,39 @@ import { Button, ConfigProvider, Form, Select } from "antd";
 
 import { useEffect, useState } from "react";
 import useMonthOptions from "../../../../components/Hooks/MessMonthDropdown";
-import { useGetActiveMonthQuery } from "../../../../redux/api/sampleApi/monthApi";
+import { useDeleteMonthMutation, useGetActiveMonthQuery } from "../../../../redux/api/sampleApi/monthApi";
 
 const DeleteOldMonth = () => {
   const [form] = Form.useForm();
   const month = useMonthOptions();
   const { data } = useGetActiveMonthQuery();
+  const [deleteMonth]=useDeleteMonthMutation();
+  
   const [months, setMonths] = useState(month);
-
 
   useEffect(() => {
     const items = month.filter((item) => item?.value !== data?._id);
     setMonths(items);
   }, [month]);
 
-  const onFinish = async (values) => {};
+  const onFinish = async ({month}) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await deleteMonth(month).unwrap();
+        if (res?.success) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        }
+      }
+    });
+  };
 
   return (
     <div>
