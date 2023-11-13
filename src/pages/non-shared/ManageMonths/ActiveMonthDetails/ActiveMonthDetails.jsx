671300a3fdffb3153fa7ca2f3ca90efdbf3cb84e
Abubakar <abubakarsiddikl.info@gmail.com>
2023-11-13 23:46:42 +0600

    active month details

--- src/pages/non-shared/ManageMonths/ActiveMonthDetails/ActiveMonthDetails.jsx
+++ src/pages/non-shared/ManageMonths/ActiveMonthDetails/ActiveMonthDetails.jsx
@@ -1,11 +1,14 @@
-import './ActiveMonthDetails.css';
+import { useGetActiveMonthQuery } from "../../../../redux/api/sampleApi/monthApi";
+import "./ActiveMonthDetails.css";
 
 const ActiveMonthDetails = () => {
-    return (
-        <div>
-            <h1>Active Month Details</h1> 
-        </div>
-    );
+  const { data } = useGetActiveMonthQuery();
+  return (
+    <div>
+      <h1>Active Month Details</h1>
+      <h1>Active Month : {data?.name}</h1>
+    </div>
+  );
 };
 
 export default ActiveMonthDetails;
