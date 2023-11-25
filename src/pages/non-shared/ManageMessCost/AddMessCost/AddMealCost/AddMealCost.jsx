22945860a224cf078949b20f1d407a5c29866712
Abubakar <abubakarsiddikl.info@gmail.com>
2023-11-25 22:28:13 +0600

    add meal design

--- src/pages/non-shared/ManageMessCost/AddMessCost/AddMealCost/AddMealCost.jsx
+++ src/pages/non-shared/ManageMessCost/AddMessCost/AddMealCost/AddMealCost.jsx
@@ -1,76 +1,96 @@
 import { IoIosArrowBack } from "react-icons/io";
 import "./AddMealCost.css";
 import { useNavigate } from "react-router-dom";
-import { Button, DatePicker, Divider, Form, Select } from "antd";
+import { Button, DatePicker, Divider, Form, Input, Select } from "antd";
 import useMemberOptions from "../../../../../components/Hooks/MembersDropdown";
 import { useState } from "react";
+import moment from "moment/moment";
 
 const AddMealCoast = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
+  // var todayDate = new Date().toISOString().slice(0, 10);
+  const [currentDate, setCurrentDate] = useState(moment());
   const [featuresList, setFeaturesList] = useState([]);
   const [newFeature, setNewFeature] = useState("");
   const members = useMemberOptions();
 
+  // console.log(currentDate);
+
   const onFinish = async ({ tags }) => {
     tags = featuresList;
   };
-  console.log(featuresList);
+  const handleDate = (e) => {
+    setCurrentDate(e.format("DD-MM-YYYY"));
+  };
   return (
     <div>
-      <div className="w-50 mx-auto">
-        
-        <Form
-          name="complex-form"
-          form={form}
-          onFinish={onFinish}
-          layout="vertical"
-          className=" serviceTable my-4 border rounded p-4"
-        >
-          <Form.Item label="Select Members">
-            <div className="mealDatePicker">
-              <h6>Date of Birth</h6>
-              <Form.Item
-                name="dateOfBirth"
-                rules={[
-                  {
-                    type: "object",
-                    required: true,
-                    message: "Please select time!",
-                  },
-                ]}
-              >
-                <DatePicker placeholder="Select Date" className="datePickerAnt" />
-              </Form.Item>
-            </div>
-
-            <Select
-              mode="multiple"
-              name="tags"
-              placeholder="Select Members"
-              onChange={(e) => setFeaturesList(e)}
-              maxTagCount={5}
-              maxTagTextLength={20}
-              dropdownRender={(menu) => (
-                <>
-                  {menu}
-                  <Divider
-                    style={{
-                      margin: "8px 0",
-                    }}
-                  />
-                </>
-              )}
-              options={members}
+      <div>
+        <h4 className="text-center mt-4">Add Meal Cost</h4>
+        <div className=" addMealCostSection  mx-auto" style={{ maxWidth: "500px" }}>
+          <div className="mealDatePicker">
+            <DatePicker
+              placeholder="Select Date"
+              className="datePickerAnt "
+              value={currentDate}
+              onChange={handleDate}
             />
-          </Form.Item>
-
-          <div className="d-flex justify-content-center  ">
-            <Button type="primary" htmlType="submit" className="w-50 h-auto">
-              <span className="fs-5"> Add</span>
-            </Button>
           </div>
-        </Form>
+          <Form
+            name="complex-form"
+            form={form}
+            onFinish={onFinish}
+            layout="vertical"
+            className="my-4"
+          >
+            <Form.Item>
+              <div >
+                <h6>Enter Cost:</h6>
+                <Form.Item
+                  name="defaultMeal"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Number!",
+                    },
+                  ]}
+                >
+                  <Input type="number" placeholder="Enter Total Meal Cost" />
+                </Form.Item>
+              </div>
+            </Form.Item>
+            <Form.Item className="addMealItemMargin">
+              <div>
+                <h6>Select Shoppers:</h6>
+                <Select
+                  mode="multiple"
+                  name="tags"
+                  placeholder="Select Members"
+                  onChange={(e) => setFeaturesList(e)}
+                  maxTagCount={5}
+                  maxTagTextLength={20}
+                  dropdownRender={(menu) => (
+                    <>
+                      {menu}
+                      <Divider
+                        style={{
+                          margin: "8px 0",
+                        }}
+                      />
+                    </>
+                  )}
+                  options={members}
+                />
+              </div>
+            </Form.Item>
+
+            <div className="d-flex justify-content-center  ">
+              <Button type="primary" htmlType="submit" className="w-50 h-auto">
+                <span className="fs-5"> Add</span>
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
       <div className="phoneBookContainer">
         <div className="phoneBookContainerMainBg">
