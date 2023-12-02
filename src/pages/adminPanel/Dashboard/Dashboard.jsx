2a9bc1f3a03cacda0c7bc13ea256401dc99ad831
Abubakar <abubakarsiddikl.info@gmail.com>
2023-12-02 17:23:36 +0600

    dashbord linked

--- src/pages/adminPanel/Dashboard/Dashboard.jsx
+++ src/pages/adminPanel/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import {
   useGetAllMonthQuery,
   useGetAllUsersQuery,
 } from "../../../redux/api/sampleApi/adminApi";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const users = useGetAllUsersQuery({ page: 1, filter: "" });
@@ -22,50 +23,52 @@ const Dashboard = () => {
       <h1 className="text-center py-4">MY DASHBOARD</h1>
       <Row className="g-2">
         <Col md={6}>
-          <div className="dashboardTotal p-4">
-            <div className="d-flex justify-content-between">
-              <h4>Total Users</h4>
+          <Link to="/mess-metrics/protected-explore/dashboard/all-users">
+            <div className="dashboardTotal p-4">
+              <div className="d-flex justify-content-between">
+                <h4>Total Users</h4>
+              </div>
+              <p className="d-flex align-items-center">
+                <span className="fs-1 me-2 text-danger">{totalUsers}</span> Users Available
+              </p>
             </div>
-            <p className="d-flex align-items-center">
-              <span className="fs-1 me-2 text-danger">{totalUsers}</span> Users Available
-            </p>
-          </div>
+          </Link>
         </Col>
         <Col md={6}>
-          <div className="dashboardTotal p-4">
-            <div className="d-flex justify-content-between">
-              <h4>Total Admins</h4>
+          <Link to="/mess-metrics/protected-explore/dashboard/all-admins">
+            <div className="dashboardTotal p-4">
+              <div className="d-flex justify-content-between">
+                <h4>Total Admins</h4>
+              </div>
+              <p className="d-flex align-items-center">
+                <span className="fs-1 me-2 text-danger">{totalAdmins}</span> Admins Available
+              </p>
             </div>
-            <p className="d-flex align-items-center">
-              <span className="fs-1 me-2 text-danger">{totalAdmins}</span> Admins Available
-            </p>
-          </div>
+          </Link>
         </Col>
         <Col md={6}>
-          <div className="dashboardTotal p-4">
-            <div className="d-flex justify-content-between">
-              <h4>
-                {/* <BsSignpost2 className="fs-1 text-primary me-1" /> */}
-                Total Mess
-              </h4>
+          <Link to="/mess-metrics/protected-explore/dashboard/all-mess">
+            <div className="dashboardTotal p-4">
+              <div className="d-flex justify-content-between">
+                <h4>Total Mess</h4>
+              </div>
+              <p className="d-flex align-items-center">
+                <span className="fs-1 me-2 text-primary">{totalMonths}</span> Mess Available
+              </p>
             </div>
-            <p className="d-flex align-items-center">
-              <span className="fs-1 me-2 text-primary">{totalMonths}</span> Mess Available
-            </p>
-          </div>
+          </Link>
         </Col>
         <Col md={6}>
-          <div className="dashboardTotal p-4">
-            <div className="d-flex justify-content-between">
-              <h4>
-                {/* <MdOutlineLocalPostOffice className="fs-1 text-warning me-1" /> */}
-                Total Months
-              </h4>
+          <Link to="/mess-metrics/protected-explore/dashboard/all-months">
+            <div className="dashboardTotal p-4">
+              <div className="d-flex justify-content-between">
+                <h4>Total Months</h4>
+              </div>
+              <p className="d-flex align-items-center">
+                <span className="fs-1 me-2 text-warning">{totalMess}</span> Months Available
+              </p>
             </div>
-            <p className="d-flex align-items-center">
-              <span className="fs-1 me-2 text-warning">{totalMess}</span> Months Available
-            </p>
-          </div>
+          </Link>
         </Col>
       </Row>
     </div>
