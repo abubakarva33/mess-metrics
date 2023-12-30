81601b9e50deeaf9a25d0d80bb8340a4e4f9f310
Abubakar <abubakarsiddikl.info@gmail.com>
2023-12-30 12:12:17 +0600

    active month swal issue

--- src/pages/non-shared/ManageMonths/ActiveMonthDetails/components/UpdateModal.jsx
+++ src/pages/non-shared/ManageMonths/ActiveMonthDetails/components/UpdateModal.jsx
@@ -11,18 +11,18 @@ import {
   useUpdateMealMutation,
   useUpdateSharedCostMutation,
 } from "../../../../../redux/api/sampleApi/actionApi";
+import SpinnerMain from "../../../../../components/Spinner/SpinnerMain";
 
 const UpdateModal = ({ data, isModalOpen, setIsModalOpen, itemName }) => {
   const [form] = Form.useForm();
   const { TextArea } = Input;
   const [value, setValue] = useState("");
-  const [res, setRes] = useState();
   const [updateBazar, { status: bazarStatus }] = useUpdateBazarMutation();
   const [updateDeposit, { status: depositStatus }] = useUpdateDepositMutation();
   const [updateIndividualCost, { status: individualCostStatus }] =
     useUpdateIndividualCostMutation();
   const [updateSharedCost, { status: sharedCostStatus }] = useUpdateSharedCostMutation();
-  const [updateMeal] = useUpdateMealMutation();
+  const [updateMeal, { status: mealStatus }] = useUpdateMealMutation();
 
   const handleOk = () => {
     setIsModalOpen(false);
@@ -38,47 +38,48 @@ const UpdateModal = ({ data, isModalOpen, setIsModalOpen, itemName }) => {
     });
   }, [data, form]);
 
-  console.log(data?._id);
+  const convertString = (inputString) => {
+    const words = inputString.split(/(?=[A-Z])/);
+    const convertedString = words
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    return convertedString;
+  };
 
   const onFinish = async (values) => {
     const body = {
       meals: [
         {
           user: data?._id,
-          meal: values.amount,
+          meal: values?.meal,
         },
       ],
       date: data?.date,
     };
-
-    console.log(body);
     const amount = Number(values.amount);
     const fieldValues = { ...values, amount };
-    console.log(fieldValues);
     Swal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this!",
+      text: `This ${convertString(itemName)} will be updated`,
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
+      confirmButtonText: "Yes, update it!",
     }).then(async (result) => {
-      if (result.isConfirmed) {
+      if (result?.isConfirmed) {
+        await setIsModalOpen(false);
         if (itemName === "meal") {
           const res = await updateMeal(body).unwrap();
-          setRes(res);
-          console.log("m c");
+          miniSwal(res);
         }
         if (itemName === "mealCost") {
           const res = await updateBazar({ id: data?._id, ...fieldValues }).unwrap();
-          setRes(res);
-          console.log("m c");
+          miniSwal(res);
         }
         if (itemName === "sharedCost") {
           const res = await updateSharedCost({ id: data?._id, ...fieldValues }).unwrap();
-          setRes(res);
-          console.log("s c");
+          miniSwal(res);
         }
         if (itemName === "individualCost") {
           const res = await updateIndividualCost({
@@ -86,42 +87,54 @@ const UpdateModal = ({ data, isModalOpen, setIsModalOpen, itemName }) => {
 
             ...fieldValues,
           }).unwrap();
-          setRes(res);
-          console.log("i c");
+          miniSwal(res);
         }
         if (itemName === "deposit") {
           const res = await updateDeposit({ id: data?._id, amount }).unwrap();
-          setRes(res);
-          console.log("d");
+          miniSwal(res);
         }
         if (itemName === "bazarCost") {
           const res = await updateBazar({ id: data?._id, ...fieldValues }).unwrap();
-          setRes(res);
-          console.log("d");
-        }
-        console.log(res);
-        if (res?.success) {
-          Swal.fire({
-            text: "Old month deleted successfully",
-            icon: "success",
-            showCancelButton: false,
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Back to Home",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              setIsModalOpen(false);
-            }
-          });
+          miniSwal(res);
         }
       }
     });
   };
-  console.log(data);
+
+  const miniSwal = async (res) => {
+    if (res?.success) {
+      console.log(res);
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      await Toast.fire({
+        icon: "success",
+        title: `${convertString(itemName)} updated successfully`,
+      });
+    }
+  };
+
+  if (
+    mealStatus == "pending" ||
+    sharedCostStatus == "pending" ||
+    individualCostStatus == "pending" ||
+    depositStatus == "pending" ||
+    bazarStatus == "pending"
+  ) {
+    return <SpinnerMain />;
+  }
+
   return (
-    <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-      <p>{data?._id}</p>
-      <p>{data?.amount}</p>
-      <h4 className="text-center mt-2 mb-4">Update Costs</h4>
+    <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} okText="Update" footer={null}>
+      <h4 className="text-center mt-2 mb-4">Update {convertString(itemName)}</h4>
       <div className="mealDatePicker">
         <ReactDatePicker
           className="w-100"
@@ -168,26 +181,31 @@ const UpdateModal = ({ data, isModalOpen, setIsModalOpen, itemName }) => {
             </div>
           </Form.Item>
         )}
-        <Form.Item>
-          <div>
-            <h6>Update Bazar List (Optional) </h6>
-            <Form.Item name="list">
-              <TextArea
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                placeholder="Enter Bazar List"
-                autoSize={{
-                  minRows: 1,
-                  maxRows: 5,
-                }}
-              />
-            </Form.Item>
-          </div>
-        </Form.Item>
+        {itemName !== "deposit" && (
+          <Form.Item>
+            <div>
+              <h6>Update Bazar List (Optional) </h6>
+              <Form.Item name="list">
+                <TextArea
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  placeholder="Enter Bazar List"
+                  autoSize={{
+                    minRows: 1,
+                    maxRows: 5,
+                  }}
+                />
+              </Form.Item>
+            </div>
+          </Form.Item>
+        )}
 
-        <div className="d-flex justify-content-center  ">
-          <Button type="primary" htmlType="submit" className="w-50 h-auto">
-            <span className="fs-5"> Add Cost</span>
+        <div className="d-flex justify-content-end">
+          <Button type="primary" htmlType="submit" className="me-3">
+            Update
+          </Button>
+          <Button className="" onClick={handleCancel}>
+            Cancel
           </Button>
         </div>
       </Form>
