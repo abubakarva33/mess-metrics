f130c0ca9aa297645fb12474319248baf2979752
Abubakar <abubakarsiddikl.info@gmail.com>
2024-01-04 23:12:26 +0600

    faq issue solved

--- src/pages/non-shared/FAQ/FAQ.jsx
+++ src/pages/non-shared/FAQ/FAQ.jsx
@@ -15,19 +15,7 @@ const FAQ = () => {
     unread: 6,
     data: [
       {
-        _id: "6591a0b209cdf52e45baa6b3",
-        message: "Bazar update 555 to 55 ",
-        isRead: false,
-        user: "658d694c22ad71943d3ee365",
-        month: "658e5b46eccb4f36e839e456",
-        manager: "658d694c22ad71943d3ee365",
-        mess: "658d695b22ad71943d3ee36e",
-        __v: 0,
-        createdAt: "2023-12-31T17:11:14.587Z",
-        updatedAt: "2023-12-31T17:11:14.587Z",
-      },
-      {
-        _id: "6591a08c09cdf52e45baa687",
+        _id: "1116591a08c09cdf52e45baa687",
         message: "Shared Cost updated 2 to 20 for undefined",
         isRead: false,
         user: "658d694c22ad71943d3ee365",
@@ -39,7 +27,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T17:10:36.896Z",
       },
       {
-        _id: "65919c1f09cdf52e45baa60c",
+        _id: "22265919c1f09cdf52e45baa60c",
         message: "Bazar update 352 to 3522 ",
         isRead: false,
         user: "658d694c22ad71943d3ee365",
@@ -51,7 +39,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T16:51:43.805Z",
       },
       {
-        _id: "65918da009cdf52e45baa4bc",
+        _id: "33365918da009cdf52e45baa4bc",
         message: "2 money expense on mess Shared cost for undefined",
         isRead: false,
         user: "658d694c22ad71943d3ee365",
@@ -63,7 +51,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:49:52.133Z",
       },
       {
-        _id: "65918d5c09cdf52e45baa490",
+        _id: "44465918d5c09cdf52e45baa490",
         message: "5 meal add on your account",
         isRead: false,
         user: "658d694c22ad71943d3ee365",
@@ -75,7 +63,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:48:44.130Z",
       },
       {
-        _id: "65918b7909cdf52e45baa40a",
+        _id: "55565918b7909cdf52e45baa40a",
         message: "10 money expense on mess Shared cost for uuh",
         isRead: false,
         user: "658d694c22ad71943d3ee365",
@@ -87,7 +75,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:40:41.648Z",
       },
       {
-        _id: "659189e309cdf52e45baa3c4",
+        _id: "666659189e309cdf52e45baa3c4",
         message: "100 money expense on mess Shared cost for undefined",
         isRead: true,
         user: "658d694c22ad71943d3ee365",
@@ -99,7 +87,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:36:14.448Z",
       },
       {
-        _id: "65918000f5daf2bfb4450488",
+        _id: "77765918000f5daf2bfb4450488",
         message: "555 money expense on mess Shared cost",
         isRead: true,
         user: "658d694c22ad71943d3ee365",
@@ -111,7 +99,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:38:21.927Z",
       },
       {
-        _id: "65917d6f1e57b6d2239041d8",
+        _id: "88865917d6f1e57b6d2239041d8",
         message: "555 money expense on mess bazar",
         isRead: true,
         user: "658d694c22ad71943d3ee365",
@@ -123,7 +111,7 @@ const FAQ = () => {
         updatedAt: "2023-12-31T15:41:44.726Z",
       },
       {
-        _id: "65917d541e57b6d2239041aa",
+        _id: "99965917d541e57b6d2239041aa",
         message: "3 meal update on your account",
         isRead: true,
         user: "658d694c22ad71943d3ee365",
@@ -136,6 +124,10 @@ const FAQ = () => {
       },
     ],
   };
+
+  const dataDivider = Math.ceil(data?.data?.length / 2);
+  const firstArray = data?.data?.slice(0, dataDivider);
+  const secondArray = data?.data?.slice(dataDivider);
   const handleCollapseChange = (key) => {
     if (openItems.includes(key)) {
       setOpenItems(openItems.filter((item) => item !== key));
@@ -145,21 +137,39 @@ const FAQ = () => {
   };
 
   return (
-    <Row>
-      {data?.data?.map((item, index) => (
-        <Col xs={12} sm={12} md={6}>
-          <Accordion
-            key={index}
-            activeKey={openItems}
-            onSelect={(key) => handleCollapseChange(key)}
-          >
-            <Accordion.Item eventKey={index.toString()}>
-              <Accordion.Header>{`Accordion ${index + 1}`}</Accordion.Header>
-              <Accordion.Body>{item?.message}</Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Col>
-      ))}
+    <Row className="mx-2">
+      <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
+        {firstArray?.map((item, index) => (
+          <div className="mb-3">
+            <Accordion
+              key={index}
+              activeKey={openItems}
+              onSelect={(key) => handleCollapseChange(key)}
+            >
+              <Accordion.Item eventKey={index.toString()}>
+                <Accordion.Header>{`Accordion ${index + 1}`}</Accordion.Header>
+                <Accordion.Body>{item?.message}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+        ))}
+      </Col>
+      <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
+        {secondArray?.map((item, index) => (
+          <div className="mb-3">
+            <Accordion
+              key={index}
+              activeKey={openItems}
+              onSelect={(key) => handleCollapseChange(key)}
+            >
+              <Accordion.Item eventKey={index.toString() + 1}>
+                <Accordion.Header>{`Accordion ${index + 1}`}</Accordion.Header>
+                <Accordion.Body>{item?.message}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+        ))}
+      </Col>
     </Row>
   );
 };
