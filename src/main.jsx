6eda4b7384bda59375acb407da20a3cfd9d543c1
suronjit797 <suronjit797@gmail.com>
2024-01-08 14:27:05 +0600

    lazy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./App.css";
+
+import React, { Suspense, lazy } from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import SkeletonLoader from "./components/SkeletonLoader/SkeletonLoader.jsx";
+
+// import App from "./App.jsx";
+const App = lazy(() => import("./App"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<SkeletonLoader />}>
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>
 );
