e6c6ea524fb83d0538ec1b844fc7e65fa43ce483
Abubakar <abubakarsiddikl.info@gmail.com>
2024-01-09 22:54:18 +0600

    revert update

--- src/redux/api/mainApi.js
+++ src/redux/api/mainApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const mainApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://mess-metrics-server.vercel.app/api/v1/",
     // baseUrl: "http://192.168.0.115:5000/api/v1/",
     // baseUrl: "http://192.168.0.110:5000/api/v1/",
     prepareHeaders: (headers, { getState }) => {
