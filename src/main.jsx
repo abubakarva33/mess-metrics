3b37c1c46f6137107a9032418832a12bb582b0de
suronjit797 <suronjit797@gmail.com>
2024-01-09 22:24:02 +0600

    before lazy

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
+// use without lazy
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
