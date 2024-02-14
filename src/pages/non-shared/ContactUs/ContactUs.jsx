4ca0e34bb779cc541711d235b8f9f2521a3b226a
Abubakar <abubakarsiddikl.info@gmail.com>
2024-02-15 00:01:50 +0600

    contact us

--- src/pages/non-shared/ContactUs/ContactUs.jsx
+++ src/pages/non-shared/ContactUs/ContactUs.jsx
@@ -1,9 +1,15 @@
+import PhoneLayout from "../../../layouts/PhoneLayout/PhoneLayout";
 import "./ContactUs.css";
 
 const ContactUs = () => {
   return (
     <div>
-      <h1>This page is in under construction</h1>
+      <div className="phonebookSection">
+        <h1>This page is in under construction</h1>
+      </div>
+      <PhoneLayout headLine="CONTACT US">
+        <h1>This page is in under construction</h1>
+      </PhoneLayout>
     </div>
   );
 };
